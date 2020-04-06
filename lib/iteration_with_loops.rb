8b4bf3ef1951360fd159a16f7a53871cef7d3815
Caroline Glass <carolineglass89@gmail.com>
2020-04-06 15:53:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,13 +4,13 @@ def join_nested_strings(src)
   row_index = 0 
   string_array = []
   while row_index < src.length do 
-    
     element_index = 0
-    while element_index < src[count].length do 
-      
+    while element_index < src[row_index].length do 
+      if src[row_index][element_index].class == String
+        string_array.push(src[row_index][element_index])
+      end 
       element_index += 1
     end 
-    
     row_index += 1
   end
     return string_array.join(" ")
